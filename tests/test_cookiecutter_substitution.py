7bf504059bf2de5ce152dfb82afb85cf37f453bd
Rolo <rolo@wildfish.com>
2015-04-30 12:30:05 +0100

    Added pytest requirements and tests for cookiecutter generation. (Pinched from @pydanny's https://github.com/pydanny/cookiecutter-django - thanks :) ).

---
+++ tests/test_cookiecutter_substitution.py
@@ -0,0 +1,34 @@
+import re
+
+import sh
+
+from .base import DjangoCookieTestCase
+
+
+class TestCookiecutterSubstitution(DjangoCookieTestCase):
+    """Test that all cookiecutter instances are substituted"""
+
+    def test_all_cookiecutter_instances_are_substituted(self):
+        # Build a list containing absolute paths to the generated files
+        paths = self.generate_project()
+
+        # Construct the cookiecutter search pattern
+        pattern = '{{(\s?cookiecutter)[.](.*?)}}'
+        re_obj = re.compile(pattern)
+
+        # Assert that no match is found in any of the files
+        for path in paths:
+            for line in open(path, 'r'):
+                match = re_obj.search(line)
+                self.assertIsNone(
+                    match,
+                    'cookiecutter variable not replaced in {}'.format(path))
+
+    def test_flake8_complaince(self):
+        """generated project should pass flake8"""
+        self.generate_project()
+        try:
+            #sh.flake8(self.destpath, "--max-line-length=120")
+            sh.flake8(self.destpath)
+        except sh.ErrorReturnCode as e:
+            raise AssertionError(e)
