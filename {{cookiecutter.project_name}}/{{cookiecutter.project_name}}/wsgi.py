3cb5d2c8e1d5264e289380804114569864e831dd
Rolo <rolo@wildfish.com>
2016-08-10 11:38:00 +0100

    Ignored pep8 failure on django-configurations wsgi.py.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/wsgi.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/wsgi.py
@@ -24,7 +24,7 @@ os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from configurations.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application  # noqa
 
 application = get_wsgi_application()
 
