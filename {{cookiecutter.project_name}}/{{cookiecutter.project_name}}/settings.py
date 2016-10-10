f175e90d9786484c7ee5e9a9f74401e4cd55f06d
Dan Bate <dan@wildfish.com>
2016-10-10 12:59:25 +0100

    removed SessionAuthenticationMiddleware

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -61,7 +61,6 @@ class Common(Configuration):
 
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
