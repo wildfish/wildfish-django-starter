dfe7153a7d21f5dfb14a4b94b8b32bde2b06f73b
Dan Bate <dan@wildfish.com>
2016-10-10 12:15:02 +0100

    changed the password validators to only include the min length validation

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -107,16 +107,7 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
     AUTH_PASSWORD_VALIDATORS = [
         {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
             'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
         },
     ]
 
