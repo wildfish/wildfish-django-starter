dc8f912cf4d9b6c703ba15f124c08d962daa68bd
Dan Bate <dan.bate@gmail.com>
2018-06-13 10:41:13 +0100

    updated to django 2.0

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.{{ cookiecutter.model_name }}List.as_view(), name='list'),
-    url(r'^new/$', views.{{ cookiecutter.model_name }}Create.as_view(), name='create'),
-    url(r'^(?P<pk>\d+)/$', views.{{ cookiecutter.model_name }}Detail.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/update/$', views.{{ cookiecutter.model_name }}Update.as_view(), name='update'),
-    url(r'^(?P<pk>\d+)/delete/$', views.{{ cookiecutter.model_name }}Delete.as_view(), name='delete'),
+    path(r'', views.{{ cookiecutter.model_name }}List.as_view(), name='list'),
+    path(r'new/', views.{{ cookiecutter.model_name }}Create.as_view(), name='create'),
+    path(r'<int:pk>/', views.{{ cookiecutter.model_name }}Detail.as_view(), name='detail'),
+    path(r'<int:pk>/update/', views.{{ cookiecutter.model_name }}Update.as_view(), name='update'),
+    path(r'<int:pk>/delete/', views.{{ cookiecutter.model_name }}Delete.as_view(), name='delete'),
 ]
+
+
+app_name = '{{ cookiecutter.app_name }}'
