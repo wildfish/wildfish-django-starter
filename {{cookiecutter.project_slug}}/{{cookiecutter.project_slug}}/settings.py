047a5bcbe05bf3d9b6def275f1a8e83586766dc4
Stuart MacKay <smackay@flagstonesoftware.com>
2018-10-17 16:07:31 +0100

    Added configuration variables.
    Added configuration variables for the following:
    
    1. domain and subdomain so the project can deal with configuration of servers.
    
    2. Django secrey key. Generating the key is a nuisance so this is going to be
       automated.
    
    3. Python version so the virtualenv for running the project can be generated
       as part of the installation process.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -257,7 +257,7 @@ class Prod(Deployed):
         }
     }
 
-    ALLOWED_HOSTS = ['.{{cookiecutter.domain_name}}', ]  # add deployment domain here
+    ALLOWED_HOSTS = ['.{{cookiecutter.domain}}', ]  # add deployment domain here
 
     RAVEN_CONFIG = {
         'dsn': '{{cookiecutter.sentry_dsn}}'
