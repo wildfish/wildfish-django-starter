d0f513e4fcb56581c3fcd8d08137f4309f168226
James Outterside <james.outterside@gmail.com>
2020-04-07 15:38:23 +0100

    Set docker db path for travis

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -12,6 +12,9 @@ cp dev-docker-compose.yml.default dev-docker-compose.yml
 pip-compile requirements.in -o requirements.txt
 
 # run the tests with this config
+
+mkdir travisdb
+DOCKER_POSTGRES_DATA=./travisdb
 docker-compose -f dev-docker-compose.yml run django pytest -vv
 
 cd ../
