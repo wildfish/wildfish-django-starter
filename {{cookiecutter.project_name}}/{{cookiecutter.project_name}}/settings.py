5e2e558d96ab36e6291ca5815889e838b6307836
James Outterside <james.outterside@gmail.com>
2020-04-08 17:31:37 +0100

    Config added for DevDocker.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -1,17 +1,21 @@
 from os import environ
-from os.path import abspath, dirname, join
-from sys import argv
+from pathlib import Path
 
+import envdir
 import sentry_sdk
 from configurations import Configuration
 from sentry_sdk.integrations.django import DjangoIntegration
 
 
+# Common settings
+BASE_DIR = Path(__file__).absolute().parent.parent
 PROJECT_NAME = "{{cookiecutter.project_name}}"
 CONFIGURATION = environ["DJANGO_CONFIGURATION"]
+CONFIG_DIR = environ.get("DJANGO_CONFIG_DIR")
+SECRET_DIR = environ.get("DJANGO_SECRET_DIR")
 
-# Detect if we are running tests.  Is this really the best way?
-IN_TESTS = "test" in argv
+# Detect if we are running tests.
+IN_TESTS = environ.get("RUNNING_TESTS")
 
 
 def get_env(name, default=None, required=False, cast=str):
@@ -39,6 +43,33 @@ def get_env(name, default=None, required=False, cast=str):
     return property(_lookup)
 
 
+def get_secret(name, cast=str):
+    """
+    Get a secret from disk
+    Secrets should be available as the content of `<SECRET_DIR>/<name>`
+    All secrets are required
+    Arguments:
+        name (str): Name of environment variable
+        cast (Callable): function to call on extracted string value
+    """
+
+    # We don't want this to be called unless we're in a configuration which uses it
+    def _lookup(self):
+        if not SECRET_DIR:
+            raise ValueError(
+                f"Secret {name} not found: DJANGO_SECRET_DIR not set in env"
+            )
+
+        file = Path(SECRET_DIR) / name
+        if not file.exists():
+            raise ValueError(f"Secret {file} not found")
+
+        value = file.read_text().strip()
+        return cast(value)
+
+    return property(_lookup)
+
+
 def csv_to_list(value):
     """
     Convert a comma separated list of values into a list.
@@ -49,33 +80,31 @@ def csv_to_list(value):
     return value.split(",")
 
 
-class RedisCache(object):
-    pass
-    # CACHES = {
-    #     "default": {
-    #         "BACKEND": "django_redis.cache.RedisCache",
-    #         "LOCATION": "redis://{}:{}/1".format(
-    #             get_env("REDIS_SERVICE_HOST", "127.0.0.1"),
-    #             get_env("REDIS_SERVICE_PORT", 6379),
-    #         ),
-    #         "KEY_PREFIX": "{}_".format(PROJECT_ENVIRONMENT_SLUG),
-    #         "OPTIONS": {
-    #             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-    #             "PARSER_CLASS": "redis.connection.HiredisParser",
-    #             # You may want this. See https://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-    #             # 'IGNORE_EXCEPTIONS': True, # see
-    #         },
-    #     }
-    # }
-    # SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-    # SESSION_CACHE_ALIAS = "default"
-
-
 class Common(Configuration):
-    PROJECT_ENVIRONMENT_SLUG = f"{PROJECT_NAME}_{CONFIGURATION}".lower()
-    BASE_DIR = dirname(dirname(abspath(__file__)))
+    @classmethod
+    def pre_setup(cls):
+        """
+        If specified, add config dir to environment
+        """
+        if CONFIG_DIR:
+            envdir.Env(CONFIG_DIR)
+        super().pre_setup()
 
-    ADMINS = (("{{cookiecutter.author_name}}", "{{cookiecutter.author_email}}"),)
+    PROJECT_ENVIRONMENT_SLUG = f"{PROJECT_NAME}_{CONFIGURATION}".lower()
+
+    @property
+    def ADMINS(self):
+        """
+        Look up DJANGO_ADMINS and split into list of (name, email) tuples
+        Separate name and email with commas, name+email pairs with semicolons, eg::
+            DJANGO_ADMINS="User One,user1@example.com;User Two,user2@example.com"
+        """
+        value = environ.get("DJANGO_ADMINS")
+        if not value:
+            return []
+
+        pairs = value.split(";")
+        return [pair.rsplit(",", 1) for pair in pairs]
 
     MANAGERS = ADMINS
 
@@ -97,7 +126,6 @@ class Common(Configuration):
         # Third party
         "whitenoise.runserver_nostatic",
         "django.contrib.staticfiles",
-        "debug_toolbar",
         "django_extensions",
         "clear_cache",
         # Project
@@ -113,7 +141,6 @@ class Common(Configuration):
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
     ]
 
     ROOT_URLCONF = "{{cookiecutter.project_name}}.urls"
@@ -121,7 +148,7 @@ class Common(Configuration):
     TEMPLATES = [
         {
             "BACKEND": "django.template.backends.django.DjangoTemplates",
-            "DIRS": [join(BASE_DIR, "templates")],
+            "DIRS": [BASE_DIR / "templates"],
             "APP_DIRS": True,
             "OPTIONS": {
                 "context_processors": [
@@ -138,13 +165,27 @@ class Common(Configuration):
 
     # Database
     # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-    # http://django-configurations.readthedocs.org/en/latest/values/#configurations.values.DatabaseURLValue
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": join(BASE_DIR, "db.sqlite3"),
+    DATABASE_HOST = get_env("DATABASE_HOST", default="localhost")
+    DATABASE_PORT = get_env("DATABASE_PORT", default=5432, cast=int)
+    DATABASE_NAME = get_env("DATABASE_NAME", default=PROJECT_NAME)
+    DATABASE_USER = get_env("DATABASE_USER", default=PROJECT_NAME)
+    DATABASE_PASSWORD = get_env("DATABASE_PASSWORD", default=PROJECT_NAME)
+
+    @property
+    def DATABASES(self):
+        """
+        Build the databases object here to allow subclasses to override specific values
+        """
+        return {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql_psycopg2",
+                "HOST": self.DATABASE_HOST,
+                "PORT": self.DATABASE_PORT,
+                "NAME": self.DATABASE_NAME,
+                "USER": self.DATABASE_USER,
+                "PASSWORD": self.DATABASE_PASSWORD,
+            }
         }
-    }
 
     # Password validation
     # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -172,51 +213,146 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.0/howto/static-files/
     STATIC_URL = "/static/"
-    STATIC_ROOT = join(BASE_DIR, "static_root")
+    STATIC_ROOT = BASE_DIR / "static"
 
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = join(BASE_DIR, "media")
+    MEDIA_ROOT = BASE_DIR / "media"
 
     # Additional locations of static files
-    STATICFILES_DIRS = [
-        join(BASE_DIR, "static"),
-    ]
+    STATICFILES_DIRS = [BASE_DIR / "frontend" / "dist"]
 
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    WHITENOISE_ROOT = BASE_DIR / "public"
 
-    FIXTURE_DIRS = [join(BASE_DIR, "fixtures")]
+    FIXTURE_DIRS = [BASE_DIR / "fixtures"]
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "verbose": {
+                "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+            },
+            "django.server": {
+                "()": "django.utils.log.ServerFormatter",
+                "format": "[%(server_time)s] %(message)s",
+            },
+        },
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "verbose",
+            },
+            "django.server": {
+                "level": "INFO",
+                "class": "logging.StreamHandler",
+                "formatter": "django.server",
+            },
+        },
+        "loggers": {
+            "django.db.backends": {
+                "level": "ERROR",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "sentry_sdk": {
+                "level": "ERROR",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "django.server": {
+                "handlers": ["django.server"],
+                "level": "INFO",
+                "propagate": False,
+            },
+        },
+    }
+
+
+class RedisCache:
+    REDIS_HOST = get_env("DJANGO_REDIS_HOST", required=True)
+    REDIS_PORT = get_env("DJANGO_REDIS_PORT", default=6379, cast=int)
+
+    # Cache
+    # https://docs.djangoproject.com/en/3.0/ref/settings/#caches
+    @property
+    def CACHES(self):
+        return {
+            "default": {
+                "BACKEND": "django_redis.cache.RedisCache",
+                "LOCATION": f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/1",
+                "KEY_PREFIX": f"{self.PROJECT_ENVIRONMENT_SLUG}_",
+                "OPTIONS": {
+                    "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                    "PARSER_CLASS": "redis.connection.HiredisParser",
+                },
+            }
+        }
 
 
 class Dev(Common):
     DEBUG = True
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = "/tmp/app-emails"
-    INTERNAL_IPS = [
-        "127.0.0.1",
-    ]
+    INTERNAL_IPS = ["127.0.0.1"]
+
+    @property
+    def INSTALLED_APPS(self):
+        INSTALLED_APPS = super().INSTALLED_APPS
+        INSTALLED_APPS.append("debug_toolbar")
+        return INSTALLED_APPS
+
+    @property
+    def MIDDLEWARE(self):
+        MIDDLEWARE = super().MIDDLEWARE
+        MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+        return MIDDLEWARE
 
 
-class Test(Dev):
+class DevDocker(RedisCache, Dev):
+    """
+    Dev for docker, uses Redis.
+    """
+
+
+class Test(Common):
     """
     Default test settings
+
+    Includes some testing speedups.
     """
 
     DEBUG = False
+    PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+    EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+
+class CI(Test):
+    """
+    Default CI settings
+    """
 
 
 class Deployed(RedisCache, Common):
     """
-    Settings which are for a non local deployment, served behind nginx.
+    Settings which are for a non-local deployment
     """
+
+    # Redefine values which are not optional in a deployed environment
+    ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS", cast=csv_to_list, required=True)
+
+    # Some deployed settings are no longer env vars - collect from the secret store
+    SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
+    DATABASE_USER = get_secret("DATABASE_USER")
+    DATABASE_PASSWORD = get_secret("DATABASE_PASSWORD")
+
+    SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+    SESSION_CACHE_ALIAS = "default"
 
     # django-debug-toolbar will throw an ImproperlyConfigured exception if DEBUG is
     # ever turned on when run with a WSGI server
     DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-    PUBLIC_ROOT = join(Common.BASE_DIR, "../public/")
-    STATIC_ROOT = join(PUBLIC_ROOT, "static")
-    MEDIA_ROOT = join(PUBLIC_ROOT, "media")
-    COMPRESS_OUTPUT_DIR = ""
 
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.sendgrid.net"
@@ -227,37 +363,17 @@ class Deployed(RedisCache, Common):
     DEFAULT_FROM_EMAIL = ""
     SERVER_EMAIL = ""
 
-    ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS", cast=csv_to_list, required=True)
-
     @classmethod
     def post_setup(cls):
         super(Deployed, cls).post_setup()
         sentry_sdk.init(
-            dsn="{{cookiecutter.sentry_dsn}}", integrations=[DjangoIntegration()],
+            dsn="{{cookiecutter.sentry_dsn}}", integrations=[DjangoIntegration()], environment=CONFIGURATION,
         )
 
 
 class Stage(Deployed):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": get_env("POSTGRES_USER", ""),
-            "USER": get_env("POSTGRES_USER", ""),
-            "PASSWORD": get_env("POSTGRES_PASSWORD", "password"),
-            "HOST": get_env("POSTGRES_SERVICE_HOST", "localhost"),
-        }
-    }
+    pass
 
 
 class Prod(Deployed):
     DEBUG = False
-
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": get_env("POSTGRES_USER", ""),
-            "USER": get_env("POSTGRES_USER", ""),
-            "PASSWORD": get_env("POSTGRES_PASSWORD", "password"),
-            "HOST": get_env("POSTGRES_SERVICE_HOST", "localhost"),
-        }
-    }
